7acac9af563ae4ec95526cbd9a04275218447127
meishijie <meishijiemeimei@163.com>
2012-07-25 12:44:13 +0800

    更新
    更新

--- adminapplication/views/index.php
+++ adminapplication/views/index.php
@@ -4,10 +4,6 @@
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
 	<script type="text/javascript" src="<?=base_url()?>js/jquery.js"></script>
 	<script type="text/javascript" src="<?=base_url()?>js/swfobject.js"></script>
- 
-	 
-	 
-
 	<title></title>
 </head>
 <body>
